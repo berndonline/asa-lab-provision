78aea3b4fdbab2bac1535e5d5bf9f0dd9e396c18
Bernd Lehmann <bernd@lehmann.be>
2017-09-16 23:13:00 +0200

    change staging.sh

--- staging.sh
+++ staging.sh
@@ -1,7 +1,8 @@
 vagrant up asa-1 --color
 vagrant up asa-2 --color
 sleep 5
-script -e -c 'ansible-playbook ../site.yml'
+export ANSIBLE_FORCE_COLOR=true
+ansible-playbook ../site.yml
 exit_code=$?
 vagrant destroy -f
 exit $exit_code
